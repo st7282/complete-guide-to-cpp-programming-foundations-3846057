6596b154c07e9eeeb0ffd1f3fb299bb3e95e67b5
st7282 <165411492+st7282@users.noreply.github.com>
2025-03-07 16:09:03 +0000

    TS Completed

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,12 @@
 // Complete Guide to C++ Programming Foundations
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
+
+#include <iostream>
+
+int main(){
+    std::cout << "Hello There" << std::endl;
+    
+    std::cout << std::endl << std::endl;
+    return 0;
+}
